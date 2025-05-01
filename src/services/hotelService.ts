105808cda2cf1ef14c401288e15ffc3cbb03fea2
Egor <agapov.em@phystech.edu>
2025-05-01 13:57:00 +0200

    backend API

--- src/services/hotelService.ts
+++ src/services/hotelService.ts
@@ -1,7 +1,9 @@
 import { Hotel, HotelResponse } from "../types/hotel";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://checkedin24-aihotelsearch.vercel.app/";
+// import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
 // Get hotels based on user prompt
 export const fetchHotels = async (
